dac1c17a0ac6bfd936807b2991683ea76bfd47b9
Michael Iyke <ymichaelc@gmail.com>
2024-01-03 19:59:30 +0100

    7. OMG WHY ARE YOU SHOUTING?

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/[A-Z]+/).join
+
+# "I realLy hOpe VancouvEr posseSs Yummy Soft vAnilla Dupper Mint Ice Nutella cream"
+# "WHAT do you SAY?"
+# "cannot read you"
