92bf1a2cf8b9d16d678f2ea48fa0431ec19fcd20
Michael Iyke <ymichaelc@gmail.com>
2024-02-24 01:01:03 +0100

    Fix alignments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Using Puppet to create a file and ensure it has a specific content
 
 file { '/tmp/school':
-	content => 'I love Puppet',
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+    ensure  => file,
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
