0dd019ab38ba8fab9c942733e514d5391244d06a
Michael Iyke <ymichaelc@gmail.com>
2024-02-24 01:19:32 +0100

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Using Puppet, install flask from pip3 and ensure version 2.1.0, flask installed
 
+package { 'python3-pip':
+    ensure => 'installed',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
 }
