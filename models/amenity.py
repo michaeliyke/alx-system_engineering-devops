980a0774039c3905244d7ba0aa776391d50fe085
Michael Iyke <ymichaelc@gmail.com>
2024-03-08 13:14:14 +0100

    cleanup

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+import sqlalchemy.orm as orm
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(129), nullable=False)
+    place_amenities = orm.relationship("Place", )
