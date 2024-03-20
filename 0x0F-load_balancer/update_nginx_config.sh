ee3ac63be2e9d973e12942d5b258329a2ac5a99d
Michael Iyke <ymichaelc@gmail.com>
2024-03-20 22:35:31 +0100

    0x0F. Load balancer - custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/update_nginx_config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+contents=$(cat <<END
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name anexe.tech www.anexe.tech;
+	add_header  X-Served-By  "$HOSTNAME";
+
+	rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+
+	error_page 404 /notfound.html;
+	location = /notfound.html {
+		root /usr/share/nginx/html; # Default but just being explicit
+		internal;
+	}
+}
+
+END
+)
+
+echo "$contents"
