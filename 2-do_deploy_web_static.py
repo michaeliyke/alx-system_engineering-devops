af5978435fa54ebc8bbf0cb0d6c386d25fa69ed5
Michael Iyke <ymichaelc@gmail.com>
2024-04-05 00:31:50 +0100

    corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,17 +18,17 @@ def do_deploy(archive_path):
     # upload to the temp dir of each server
     put(archive_path, "/tmp")
     # uncompress to /data/web_static/releases/archive_name
-    run("mkdir -p /data/web_static/releases/{}".format(path.stem))
-    run("rm -rf /data/web_static/releases/{}/*".format(path.stem))
-    run("tar -xzf /tmp/{}  -C /data/web_static/releases/{}"
+    run("sudo mkdir -p /data/web_static/releases/{}".format(path.stem))
+    run("sudo rm -rf /data/web_static/releases/{}/*".format(path.stem))
+    run("sudo tar -xzf /tmp/{}  -C /data/web_static/releases/{}"
         .format(path.name, path.stem))
-    run("mv /data/web_static/releases/{}/web_static/*\
+    run("sudo mv /data/web_static/releases/{}/web_static/*\
             /data/web_static/releases/{}".format(path.stem, path.stem))
-    run("rmdir /data/web_static/releases/{}/web_static".format(path.stem))
+    run("sudo rmdir /data/web_static/releases/{}/web_static".format(path.stem))
     # delete archive from /tmp/
-    run("rm /tmp/{}".format(path.name))
+    run("sudo rm /tmp/{}".format(path.name))
     # recreate the symlink /data/web_static/current and point to archive_name
-    run("rm -f /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}  /data/web_static/current"
+    run("sudo rm -f /data/web_static/current")
+    run("sudo ln -s /data/web_static/releases/{}  /data/web_static/current"
         .format(path.stem))
     return True
