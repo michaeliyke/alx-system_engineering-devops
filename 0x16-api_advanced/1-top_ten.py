e0d39da62875e3fde02b56155d46189556cd24b5
Michael Iyke <ymichaelc@gmail.com>
2024-05-08 10:15:45 +0100

    correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,23 +7,28 @@ headers = {'User-Agent': 'student-task: u/Aneze'}
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        return response.json().get('data').get('subscribers')
+    except Exception as e:
         return 0
-    return response.json().get('data').get('subscribers')
 
 
 def top_ten(subreddit):
     """Get the top 10 hot posts for a given subreddit"""
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+        data = response.json().get('data')
+        posts = data.get('children')
+        for post in posts[:10]:
+            print(post.get('data').get('title'))
+    except Exception as e:
         print(None)
-        return
-    data = response.json().get('data')
-    posts = data.get('children')
-    for post in posts[:10]:
-        print(post.get('data').get('title'))
