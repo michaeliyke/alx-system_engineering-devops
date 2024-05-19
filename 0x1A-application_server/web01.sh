4276be36d94e1eeb388e0b16b93d6b3357f85bf1
Michael Iyke <ymichaelc@gmail.com>
2024-05-19 19:26:45 +0100

    Task 5

--- 0x1A-application_server/web01.sh
+++ 0x1A-application_server/web01.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-# Script to start the gunicorn server serving task 6 of web_flask
-gunicorn --bind 0.0.0.0:5001 web_flask.6-number_odd_or_even:app
+#+ # Script to start the gunicorn server serving task 0 of web_flask
+#+ # Environment variables here
+HBNB_ENV=test
+HBNB_MYSQL_USER=hbnb_test
+HBNB_MYSQL_PWD=hbnb_test_pwd
+HBNB_MYSQL_HOST=localhost
+HBNB_MYSQL_DB=hbnb_test_db
+HBNB_TYPE_STORAGE=db
+
+gunicorn --bind 0.0.0.0:5003 web_dynamic.2-hbnb:app
 
